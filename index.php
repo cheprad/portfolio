4adcf0da2cf52cc52e489ed1943f9fc21f22fc0f
cheprad <cheprad123@gmail.com>
2021-01-31 18:30:11 +0700

    add php file

---
+++ index.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.6.3/css/foundation.min.css" integrity="sha512-dMUQinc1gbNb95AFtKtP4q/g56T8r9oDxlWy0hrHyzfUbMq/vQztAqaR/FDY/bY0R1Ikc30aq94jyQH2Ix++ug==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/foundation/6.6.3/css/foundation-float.min.css" integrity="sha512-tuBWy51BW5GZ5BfYddst4eq3d8mcnOk1I5ZHj45P5AUcf9yo5X3KmOJslloaM0ZoSDzS4lcGY91j3L1k5ZVuVg==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+    <div class="container">
+        <h1 class="name">che gave</h1>
+        <span class="slogan">class / job / what u do </span>
+        <div class="social-img">
+            <ul>
+                <li><a href="#" target="_blank"><i class="fa tw fa-twitter"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa tw fa-linkedin"></i></a></li>
+                <li><a href="#" target="_blank"><i class="fa tw fa-github"></i></a></li>
+            </ul>
+        </div>
+        <p class="bio">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Reprehenderit ipsam nihil, itaque repellat tempore maxime fuga voluptatum, blanditiis voluptatem inventore dolor! Quis deserunt voluptas doloremque cumque in sit ut soluta, vero eligendi ducimus, sunt quod eos ullam quam voluptatibus debitis pariatur ea animi voluptatem non delectus reprehenderit aperiam similique! Ipsam, cumque similique delectus fugit vel officia, totam, quae et commodi fuga iste! Harum ex enim repellendus, facilis quasi id ratione neque illum? Illum maxime ipsa sint odio. Voluptatum dolorum temporibus modi officiis eaque, perferendis, praesentium repellat officia eos cum magni ad nisi totam fuga accusamus ab fugit vel neque repellendus.
+        </p>
+        <h5>Portfolio</h5>
+        <hr>
+        <div class="row">
+            <div class="small-6 columns">
+                <div class="work">
+                    <img src="http://playserver.in.th/user_image/server_icon/10878_1901484633.gif">
+                </div>
+            </div>
+            <div class="small-6 columns">
+                <div class="work">
+                    <img src="http://playserver.in.th/user_image/server_icon/10878_1901484633.gif">
+                </div>
+            </div>
+            <div class="small-6 columns">
+                <div class="work">
+                    <img src="http://playserver.in.th/user_image/server_icon/10878_1901484633.gif">
+                </div>
+            </div>
+            <div class="small-6 columns">
+                <div class="work">
+                    <img src="http://playserver.in.th/user_image/server_icon/10878_1901484633.gif">
+                </div>
+            </div>
+        </div>
+        <h5>Contact Me</h5>
+        <hr>
+        <form action="">
+            <div class="row">
+                <div class="medium-6 columns">
+                    <label for="name">Name:
+                        <input type="text" placeholder="Your name">
+                    </label>
+                </div>
+                <div class="medium-6 columns">
+                    <label for="name">Email:
+                        <input type="text" placeholder="Your e-mail">
+                    </label>
+                </div>
+                <div class="medium-12 columns">
+                    <label for="message">Message
+                        <textarea name="message" placeholder="None"></textarea>
+                    </label>
+                    <button class="button">Send</button>
+                </div>
+            </div>
+            <div class="footer">
+                test
+                <script>
+                    document.write(new Date().getFullYear())
+                </script> test 2
+            </div>
+        </form>
+    </div>
+</body>
+</html>
